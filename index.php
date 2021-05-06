da5fa684b6cfa31070d0a2d76bc7512f132d303e
Mati <matirezzone6@gmail.com>
2021-05-06 11:00:05 +0300

    Intial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Smart Iriigation System </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="index.css" rel="stylesheet"/>
+    </head>
+
+    <body>
+        <header>
+            <div id="container">
+                <div>
+                    <h2>Smart Irrigation System</h2>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <section id="list_data_section">
+                Here the data will be go here.
+            </section>
+
+            <section id="detail_data_section">
+                The graph and extra thing will be put.
+            </section>
+            <section>
+dhdj
+
+            </section>
+        </main>
+    </body>
+</html>
